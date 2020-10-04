cde2eb1dcdd481aa69412f69e56ab72f7dafc641
Parth00 <dubalparth@gmail.com>
2020-10-04 16:41:09 -0400

    Authorization added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './components/Login/Login';
+import { getTokenFromUrl } from './components/Login/spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log('TOKEN >>>>>>>>>>> ', _token)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        token ? (
+          // <Player/>
+          <h1>I am logged in</h1>
+        ) : (
+            <Login />
+          )
+      }
+
     </div>
   );
 }
